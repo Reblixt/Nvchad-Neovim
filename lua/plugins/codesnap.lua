c23f789b6a5d4257af671821a64dc6530859d30b
Reblixt <carl@klofverskjold.se>
2024-12-14 16:09:10 +0100

    upadte

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,21 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make build_generator",
+  keys = {
+    { "<leader>cc", "<cmd>CodeSnap<cr>",     mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+  },
+  opts = {
+    mac_window_bar = false,
+    save_path = "~/Pictures",
+    has_breadcrumbs = true,
+    bg_padding = 0,
+    -- code_font_family = "FiraCode Nerd Font Bold",
+    -- watermark_font_family = "FiraCode Nerd Font Bold",
+    title = "Reblixt",
+    watermark = "Reblixt",
+    has_line_number = true,
+    -- show_workspace = true,
+    -- bg_theme = "",
+  },
+}
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,40 +1,40 @@
+-- return {}
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			lsp = {
-				-- Inaktivera helt LSP-relaterade funktioner i Noice
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-					["vim.lsp.util.stylize_markdown"] = false,
-					["cmp.entry.get_documentation"] = false,
-				},
-				-- Inaktivera signaturfunktionen
-				signature = {
-					enabled = false,
-				},
-				-- Inaktivera hover
-				hover = {
-					enabled = false,
-				},
-			},
-			-- Inaktivera förhandsvisning av kommandon
-			presets = {
-				lsp_doc_border = false,
-			},
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			require("notify").setup({
-				background_colour = "#000000",
-			})
-		end,
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      -- Inaktivera helt LSP-relaterade funktioner i Noice
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+        ["vim.lsp.util.stylize_markdown"] = false,
+        ["cmp.entry.get_documentation"] = false,
+      },
+      -- Inaktivera signaturfunktionen
+      signature = {
+        enabled = false,
+      },
+      -- Inaktivera hover
+      hover = {
+        enabled = true,
+      },
+    },
+    -- Inaktivera förhandsvisning av kommandon
+    presets = {
+      lsp_doc_border = false,
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    -- "rcarriga/nvim-notify",
+  },
 }
+-- {
+-- 	"rcarriga/nvim-notify",
+-- 	config = function()
+-- 		require("notify").setup({
+-- 			background_colour = "#000000",
+-- 		})
+-- 	end,
+-- },
+-- }
